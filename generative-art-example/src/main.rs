c02ee0ee647949dfad4703e90ada77b370d0aebe
Mickaël Gregot <mickaelg@theodo.fr>
2023-04-18 08:36:47 +0200

    2 - draw one diagonal

--- generative-art-example/src/main.rs
+++ generative-art-example/src/main.rs
@@ -1,7 +1,11 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .size(900, 900)
+        .run();
 }
 
 struct Model {}
@@ -12,6 +16,16 @@ fn model(_app: &App) -> Model {
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+fn view(app: &App, _model: &Model, frame: Frame) {
+    frame.clear(WHITE);
+
+    let draw = app.draw();
+
+    draw.line()
+        .start(app.window_rect().bottom_right())
+        .end(app.window_rect().top_left())
+        .weight(10.0)
+        .color(BLACK);
+
+    draw.to_frame(app, &frame).unwrap();
 }
