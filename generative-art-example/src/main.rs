d6f62a343b44db7f9db48fe78e1687eaf1447cd2
Mickaël Gregot <mickaelg@theodo.fr>
2023-06-20 10:24:03 +0200

    Article - 01 nominal sketch

--- generative-art-example/src/main.rs
+++ generative-art-example/src/main.rs
@@ -1,17 +1,12 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::sketch(view).run()
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(NAVY);
+    draw.ellipse().color(MEDIUMAQUAMARINE);
+    draw.to_frame(app, &frame).unwrap();
 }
